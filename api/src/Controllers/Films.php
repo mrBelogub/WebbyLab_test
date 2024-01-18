eb1704a0178cc10b0ef33c0a1b3130f523c75f69
Сергей Белогуб <s.belogub@alef.im>
2024-01-18 19:15:59 +0200

    Додав видачу по назвi

--- api/src/Controllers/Films.php
+++ api/src/Controllers/Films.php
@@ -2,23 +2,25 @@
 
 class Films{
 
-    public static function getWithoutSorting(){
+    public static function getWithoutSorting($title = null){
         $films_list = DB::q("SELECT `films`.*, GROUP_CONCAT(`stars`.`name` SEPARATOR ', ') AS `actors`
                             FROM `films`
                             LEFT JOIN `stars_in_films` ON `films`.`id` = `stars_in_films`.`film_id`
                             LEFT JOIN `stars` ON `stars`.`id` = `stars_in_films`.`star_id`
-                            GROUP BY `films`.`id`", []);
+                            WHERE `films`.`title` LIKE COALESCE(CONCAT('%', :title, '%'), `films`.`title`) 
+                            GROUP BY `films`.`id`", ["title" => $title]);
 
         return $films_list;   
     }
 
-    public static function getByAlphabeticalTitle(){
+    public static function getByAlphabeticalTitle($title = null){
         $films_list = DB::q("SELECT `films`.*, GROUP_CONCAT(`stars`.`name` SEPARATOR ', ') AS `actors`
                             FROM `films`
                             LEFT JOIN `stars_in_films` ON `films`.`id` = `stars_in_films`.`film_id`
                             LEFT JOIN `stars` ON `stars`.`id` = `stars_in_films`.`star_id`
+                            WHERE `films`.`title` LIKE COALESCE(CONCAT('%', :title, '%'), `films`.`title`) 
                             GROUP BY `films`.`id`
-                            ORDER BY `films`.`title` ASC", []);
+                            ORDER BY `films`.`title` ASC", ["title" => $title]);
 
         return $films_list;   
     }
--- api/src/get_films.php
+++ api/src/get_films.php
@@ -2,13 +2,15 @@
 
 $sort_type = $_GET['sort_type'] ?? null;
 
+$title = $_GET['title'] ?? null;
+
 switch ($sort_type) {
     case SORT_TYPE_APLHABETICAL:
-        $films_list = Films::getByAlphabeticalTitle();
+        $films_list = Films::getByAlphabeticalTitle($title);
         break;
     
     default:
-        $films_list = Films::getWithoutSorting();
+        $films_list = Films::getWithoutSorting($title);
         break;
 }
 
