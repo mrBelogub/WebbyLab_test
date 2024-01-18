186c145353d2f2e7705c102447da564ccb932ecb
Сергей Белогуб <s.belogub@alef.im>
2024-01-18 19:27:44 +0200

    Додав видачу по акторам

--- api/src/Controllers/Films.php
+++ api/src/Controllers/Films.php
@@ -2,25 +2,26 @@
 
 class Films{
 
-    public static function getWithoutSorting($title = null){
+    public static function getWithoutSorting($title = null, $actor_name = null){
         $films_list = DB::q("SELECT `films`.*, GROUP_CONCAT(`stars`.`name` SEPARATOR ', ') AS `actors`
                             FROM `films`
                             LEFT JOIN `stars_in_films` ON `films`.`id` = `stars_in_films`.`film_id`
                             LEFT JOIN `stars` ON `stars`.`id` = `stars_in_films`.`star_id`
-                            WHERE `films`.`title` LIKE COALESCE(CONCAT('%', :title, '%'), `films`.`title`) 
-                            GROUP BY `films`.`id`", ["title" => $title]);
-
+                            WHERE `films`.`title` LIKE COALESCE(CONCAT('%', :title, '%'), `films`.`title`)
+                            AND `stars`.`name` LIKE COALESCE(CONCAT('%', :actor_name, '%'), `stars`.`name`)
+                            GROUP BY `films`.`id`", ["title" => $title, "actor_name" => $actor_name]);
         return $films_list;   
     }
 
-    public static function getByAlphabeticalTitle($title = null){
+    public static function getByAlphabeticalTitle($title = null, $actor_name = null){
         $films_list = DB::q("SELECT `films`.*, GROUP_CONCAT(`stars`.`name` SEPARATOR ', ') AS `actors`
                             FROM `films`
                             LEFT JOIN `stars_in_films` ON `films`.`id` = `stars_in_films`.`film_id`
                             LEFT JOIN `stars` ON `stars`.`id` = `stars_in_films`.`star_id`
                             WHERE `films`.`title` LIKE COALESCE(CONCAT('%', :title, '%'), `films`.`title`) 
+                            AND `stars`.`name` LIKE COALESCE(CONCAT('%', :actor_name, '%'), `stars`.`name`)
                             GROUP BY `films`.`id`
-                            ORDER BY `films`.`title` ASC", ["title" => $title]);
+                            ORDER BY `films`.`title` ASC", ["title" => $title, "actor_name" => $actor_name]);
 
         return $films_list;   
     }
--- api/src/get_films.php
+++ api/src/get_films.php
@@ -4,13 +4,15 @@ $sort_type = $_GET['sort_type'] ?? null;
 
 $title = $_GET['title'] ?? null;
 
+$actor_name = $_GET['actor_name'] ?? null;
+
 switch ($sort_type) {
     case SORT_TYPE_APLHABETICAL:
-        $films_list = Films::getByAlphabeticalTitle($title);
+        $films_list = Films::getByAlphabeticalTitle($title, $actor_name);
         break;
     
     default:
-        $films_list = Films::getWithoutSorting($title);
+        $films_list = Films::getWithoutSorting($title, $actor_name);
         break;
 }
 
